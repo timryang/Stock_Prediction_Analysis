64c8c4f33e00fd3ecf4df2c0d8025229138436aa
timryang <timothyyang94@gmail.com>
2020-05-26 08:55:57 -0400

    Updated deploy and main to make it easier to update

--- deploy.py
+++ deploy.py
@@ -23,13 +23,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/define_parameters', methods=['GET', 'POST'])
 def define_parameters():
-    return render_template('results.html', ticker='TSLA', years=1,\
+    return render_template('results.html', ticker='MRNA', years=1,\
                            geoLocation='', distance='', sinceDate='2020-03-01', untilDate='2020-05-20',\
-                               querySearch='TSLA', topTweets=True, maxTweets=500,\
+                               querySearch='MRNA', topTweets=True, maxTweets=500,\
                                    deltaInterval=3, trainSize=0.8,\
                                        useIDF=True, do_downsample=True, useStopwords=True, addStopwords='',\
                                            recollectTweets=True, recollectData=True,\
-                                           geoLocationPredict='', distancePredict='', querySearchPredict='TSLA',\
+                                           geoLocationPredict='', distancePredict='', querySearchPredict='MRNA',\
                                                topTweetsPredict=True, maxTweetsPredict=10)
 
 @app.route('/results', methods=['GET', 'POST'])
